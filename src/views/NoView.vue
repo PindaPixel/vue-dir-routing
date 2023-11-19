6c0185607dd778b5f76475de6c07668b4991ae7e
Lucas <pindapixel@outlook.com>
2023-11-19 13:01:10 +0100

    feat: implement auto router

---
+++ src/views/NoView.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import { UnauthorizedError } from '@/router/error';
+
+    throw new UnauthorizedError("Access denied.");
+</script>
+<template>
+    xD
+</template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,83 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import { configDefaults } from 'vitest/config'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import { configDefaults } from "vitest/config";
+import vue from "@vitejs/plugin-vue";
+import VueRouter from "unplugin-vue-router/vite";
+import { RouteRecordRaw } from "vue-router";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [
+        VueRouter({
+            routesFolder: [
+                {
+                    src: "src/views",
+                    extensions: [".vue"],
+                    filePatterns: "**/*.layout",
+                    path(filepath) {
+                        return filepath
+                            .slice(
+                                filepath.lastIndexOf("src/views") +
+                                    "src/views".length
+                            )
+                            .replace(/\/?_\w+\.layout\.vue/g, ".vue");
+                    },
+                },
+                {
+                    src: "src/views",
+                    extensions: [".vue"],
+                    filePatterns: "**/*View",
+                    path(filepath) {
+                        return filepath
+                            .slice(
+                                filepath.lastIndexOf("src/views") +
+                                    "src/views".length
+                            )
+                            .replace("View.vue", "/index.vue")
+                            .toLowerCase()
+                            .replace("/home", "");
+                    },
+                },
+                {
+                    src: "src/views",
+                    extensions: [".vue"],
+                    filePatterns: "**/*.layout",
+                    path(filepath) {
+                        return filepath
+                            .slice(
+                                filepath.lastIndexOf("src/views") +
+                                    "src/views".length
+                            )
+                            .replace(/\/?_\w+\.layout\.vue/g, "/[...index]");
+                    },
+                },
+            ],
+            importMode(filepath) {
+                return filepath.indexOf(".layout.vue") > -1 ||
+                    filepath === "@/components/NotFound.vue"
+                    ? "sync"
+                    : "async";
+            },
+            extendRoute(route) {
+                if (route.path !== ":index(.*)") return;
+
+                route.components.set("default", "@/components/NotFound.vue");
+            },
+        }),
+        vue(),
+    ],
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
     },
-  },
-  build: {
-    manifest: true
-  },
-  test: {
-    environment: 'jsdom',
-    exclude: [...configDefaults.exclude, 'e2e/*'],
-    root: fileURLToPath(new URL('./', import.meta.url))
-  }
-})
+    build: {
+        manifest: true,
+    },
+    test: {
+        environment: "jsdom",
+        exclude: [...configDefaults.exclude, "e2e/*"],
+        root: fileURLToPath(new URL("./", import.meta.url)),
+    },
+});
